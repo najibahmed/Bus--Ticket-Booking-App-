ce7b6046c939a89c58a5d79e1c76d522450cd862
Najib <najibahmed50@gmail.com>
2022-10-21 12:41:34 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:bus_ticket_booking_app/Pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ticket Booking ',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: LoginPage.routename,
+      routes: {
+        LoginPage.routename:(context)=> const LoginPage(),
+      },
+    );
+  }
+}
+
+
